c4b392228743a7ae78511b831cceb2024a0d04f6
Tony Campbell <tcampbell@github.com>
2018-06-28 22:20:49 -0400

    Add comparisons for all poker hands

--- pai_gow/poker_test.py
+++ pai_gow/poker_test.py
@@ -6,26 +6,32 @@ import poker
 
 class PokerScoringTestCase(unittest.TestCase):
 
-    def testFiveOfAKind(self):
+    def testFiveOfaKind(self):
         self.assertTrue(poker.FIVE_OFA_KIND > poker.STRAIGHT_FLUSH)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.FOUR_OFA_KIND)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.FULL_HOUSE)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.FLUSH)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.STRAIGHT)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.THREE_OFA_KIND)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.TWO_PAIR)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.PAIR)
-        self.assertTrue(poker.FIVE_OFA_KIND > poker.HIGH_CARD)
 
     def testStraightFlush(self):
         self.assertTrue(poker.STRAIGHT_FLUSH > poker.FOUR_OFA_KIND)
-        self.assertTrue(poker.STRAIGHT_FLUSH > poker.FULL_HOUSE)
-        self.assertTrue(poker.STRAIGHT_FLUSH > poker.FLUSH)
-        self.assertTrue(poker.STRAIGHT_FLUSH > poker.STRAIGHT)
-        self.assertTrue(poker.STRAIGHT_FLUSH > poker.THREE_OFA_KIND)
-        self.assertTrue(poker.STRAIGHT_FLUSH > poker.TWO_PAIR)
-        self.assertTrue(poker.STRAIGHT_FLUSH > poker.PAIR)
-        self.assertTrue(poker.STRAIGHT_FLUSH > poker.HIGH_CARD)
+
+    def testFourOfaKind(self):
+        self.assertTrue(poker.FOUR_OFA_KIND > poker.FULL_HOUSE)
+
+    def testFullHouse(self):
+        self.assertTrue(poker.FULL_HOUSE > poker.FLUSH)
+
+    def testFlush(self):
+        self.assertTrue(poker.FLUSH > poker.STRAIGHT)
+
+    def testStraight(self):
+        self.assertTrue(poker.STRAIGHT > poker.THREE_OFA_KIND)
+
+    def testThreeOfaKind(self):
+        self.assertTrue(poker.THREE_OFA_KIND > poker.TWO_PAIR)
+
+    def testTwoPair(self):
+        self.assertTrue(poker.TWO_PAIR > poker.PAIR)
+
+    def testPair(self):
+        self.assertTrue(poker.PAIR > poker.HIGH_CARD)
 
 
 if '__main__' == __name__:
