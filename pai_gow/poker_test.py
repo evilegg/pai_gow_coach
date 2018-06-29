9297aaf4fdb073888055b8cd985011968aa15088
Tony Campbell <tcampbell@github.com>
2018-06-29 11:33:33 -0400

    Rank comparisons ignore suit

--- pai_gow/poker_test.py
+++ pai_gow/poker_test.py
@@ -64,7 +64,7 @@ class PokerHandScoringTestCase(unittest.TestCase):
         self.assertTrue(poker.Pair() > poker.HighCard())
 
 
-class PokerHandTestCase(unittest.TestCase):
+class FlushTestCase(unittest.TestCase):
     def testFlush(self):
         self.assertIsInstance(poker.hand('As 3s 4s 5s 6s'), poker.Flush)
 
@@ -87,6 +87,47 @@ class PokerCardTestCase(unittest.TestCase):
         self.assertTrue(poker.Card('5s') > poker.Card('4s'))
         self.assertTrue(poker.Card('4s') > poker.Card('3s'))
         self.assertTrue(poker.Card('3s') > poker.Card('2s'))
+
+    def testRanksIgnoreSuits(self):
+        self.assertTrue(poker.Card('As') == poker.Card('Ac')
+                                         == poker.Card('Ad')
+                                         == poker.Card('Ah'))
+        self.assertTrue(poker.Card('Ks') == poker.Card('Kc')
+                                         == poker.Card('Kd')
+                                         == poker.Card('Kh'))
+        self.assertTrue(poker.Card('Qs') == poker.Card('Qc')
+                                         == poker.Card('Qd')
+                                         == poker.Card('Qh'))
+        self.assertTrue(poker.Card('Js') == poker.Card('Jc')
+                                         == poker.Card('Jd')
+                                         == poker.Card('Jh'))
+        self.assertTrue(poker.Card('Ts') == poker.Card('Tc')
+                                         == poker.Card('Td')
+                                         == poker.Card('Th'))
+        self.assertTrue(poker.Card('9s') == poker.Card('9c')
+                                         == poker.Card('9d')
+                                         == poker.Card('9h'))
+        self.assertTrue(poker.Card('8s') == poker.Card('8c')
+                                         == poker.Card('8d')
+                                         == poker.Card('8h'))
+        self.assertTrue(poker.Card('7s') == poker.Card('7c')
+                                         == poker.Card('7d')
+                                         == poker.Card('7h'))
+        self.assertTrue(poker.Card('6s') == poker.Card('6c')
+                                         == poker.Card('6d')
+                                         == poker.Card('6h'))
+        self.assertTrue(poker.Card('5s') == poker.Card('5c')
+                                         == poker.Card('5d')
+                                         == poker.Card('5h'))
+        self.assertTrue(poker.Card('4s') == poker.Card('4c')
+                                         == poker.Card('4d')
+                                         == poker.Card('4h'))
+        self.assertTrue(poker.Card('3s') == poker.Card('3c')
+                                         == poker.Card('3d')
+                                         == poker.Card('3h'))
+        self.assertTrue(poker.Card('2s') == poker.Card('2c')
+                                         == poker.Card('2d')
+                                         == poker.Card('2h'))
 
 
 if '__main__' == __name__:
