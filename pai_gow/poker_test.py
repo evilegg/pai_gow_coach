e3cc6c60ee54941a1a0d1490ffb90edb8b71aa98
Tony Campbell <tcampbell@github.com>
2018-07-03 16:55:44 -0400

    Fix unnecessary whitespace

--- pai_gow/poker_test.py
+++ pai_gow/poker_test.py
@@ -7,36 +7,28 @@ from . import poker
 class PokerEnumScoringTestCase(unittest.TestCase):
 
     def testStraightFlush(self):
-        self.assertTrue(poker.Ranks.STRAIGHT_FLUSH >
-                        poker.Ranks.FOUR_OFA_KIND)
+        self.assertTrue(poker.Ranks.STRAIGHT_FLUSH > poker.Ranks.FOUR_OFA_KIND)
 
     def testFourOfaKind(self):
-        self.assertTrue(poker.Ranks.FOUR_OFA_KIND >
-                        poker.Ranks.FULL_HOUSE)
+        self.assertTrue(poker.Ranks.FOUR_OFA_KIND > poker.Ranks.FULL_HOUSE)
 
     def testFullHouse(self):
-        self.assertTrue(poker.Ranks.FULL_HOUSE >
-                        poker.Ranks.FLUSH)
+        self.assertTrue(poker.Ranks.FULL_HOUSE > poker.Ranks.FLUSH)
 
     def testFlush(self):
-        self.assertTrue(poker.Ranks.FLUSH >
-                        poker.Ranks.STRAIGHT)
+        self.assertTrue(poker.Ranks.FLUSH > poker.Ranks.STRAIGHT)
 
     def testStraight(self):
-        self.assertTrue(poker.Ranks.STRAIGHT >
-                        poker.Ranks.THREE_OFA_KIND)
+        self.assertTrue(poker.Ranks.STRAIGHT > poker.Ranks.THREE_OFA_KIND)
 
     def testThreeOfaKind(self):
-        self.assertTrue(poker.Ranks.THREE_OFA_KIND >
-                        poker.Ranks.TWO_PAIR)
+        self.assertTrue(poker.Ranks.THREE_OFA_KIND > poker.Ranks.TWO_PAIR)
 
     def testTwoPair(self):
-        self.assertTrue(poker.Ranks.TWO_PAIR >
-                        poker.Ranks.PAIR)
+        self.assertTrue(poker.Ranks.TWO_PAIR > poker.Ranks.PAIR)
 
     def testPair(self):
-        self.assertTrue(poker.Ranks.PAIR >
-                        poker.Ranks.HIGH_CARD)
+        self.assertTrue(poker.Ranks.PAIR > poker.Ranks.HIGH_CARD)
 
 
 class PokerHandScoringTestCase(unittest.TestCase):
