214d831a9e670b006466c1b661903826535cdf71
Tony Campbell <93364+evilegg@users.noreply.github.com>
2023-07-03 14:07:58 -0400

    Fix card comparison methods and get tests to pass

--- pai_gow/cards.py
+++ pai_gow/cards.py
@@ -4,18 +4,39 @@
 class Card(object):
     RANKS = dict(reversed(e) for e in enumerate(
                  '2 3 4 5 6 7 8 9 T J Q K A'.split()))
+    RANKS = {
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        'T': 10,
+        'J': 11,
+        'Q': 12,
+        'K': 13,
+        'A': 14,
+    }
+    SUITS = set('scdh')
 
     def __init__(self, card_spec):
         rank, suit = card_spec
         assert rank in self.RANKS
-        self.rank = self.RANKS[rank]
+        assert suit in self.SUITS
+        self.rank = rank
         self.suit = suit
-        self._raw_rank = rank
 
-    def __cmp__(self, other):
-        return cmp(self.rank, other.rank)
+    def score(self):
+        return self.RANKS[self.rank]
+
+    def __eq__(self, other_card):
+        return (self.score() == other_card.score())
+
+    def __lt__(self, other_card):
+        return (self.score() < other_card.score())
 
     def __repr__(self):
-        return '%s("%s%s")' % (self.__class__.__name__,
-                               self._raw_rank,
-                               self.suit)
+        return '%s("%s%s")' % (self.__class__.__name__, self.rank, self.suit)
+
